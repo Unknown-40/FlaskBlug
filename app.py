49ab0bdd2a16da616dffd4d64716cd6b53e15a8f
Unknown-21-m <mansourosmani4050mnr@gmail.com>
2020-12-29 10:57:01 +0430

    Register admin Blueprint to the main app

--- app.py
+++ app.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def index():
     return "Hello World!"
 
+
+from mode_admin import admin
+
+app.register_blueprint(admin)
+
+
+
+
 #git init
 #git remote add origin address github
+#  pip3 freeze > requirements.txt
+#  git config --global user.email "mansourosmani4050mnr@gmail.com"
+#  git config --global user.name "Unknown-21-m"
+#  git -m commit "Initialize Project"
+#  git push --set-upstream origin master
+#  git push
+
+
+# to add a file and update
+# git add filename
+# git commit -m "Update filename"
+# git push
